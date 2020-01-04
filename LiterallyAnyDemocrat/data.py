b8d48ed376e4889dccb5a29a8c70be6f62a43531
Michal Migurski <mike@teczno.com>
2020-01-04 12:06:53 -0800

    Added state parsing

--- LiterallyAnyDemocrat/data.py
+++ LiterallyAnyDemocrat/data.py
@@ -1,9 +1,13 @@
-import unittest, collections, datetime
+import collections, datetime, requests, io, csv
+import unittest, httmock
 
 # Ballotpedia conventionally uses an '(i)' next to candidate names
 IMARK = ' (i)'
 
 Candidate = collections.namedtuple('Candidate', ('name', 'incumbent'))
+
+State = collections.namedtuple('State', ('state', 'chamber', 'reason',
+    'filing_deadline', 'primary_election', 'weight', 'detail_url'))
 
 class TestData (unittest.TestCase):
     
@@ -29,6 +33,29 @@ class TestData (unittest.TestCase):
 
         d3 = parse_date('August 18, 2020')
         self.assertEqual(d3, datetime.date(2020, 8, 18))
+    
+    def test_parse_number(self):
+        self.assertEqual(parse_number('14,350,000'), 14350000)
+        self.assertEqual(parse_number(''), 0)
+    
+    def test_load_states(self):
+        def mock_requests(url, request):
+            return b'State,Chamber,Reason,Filing Deadline,Primary Candidates,Primary Election,General Candidates,Weight,Race Detail\nTexas,House of Representatives,Redistricting,"December 9, 2019",Yes,"March 3, 2020",,"191,333","https://ballotpedia.org/Texas_House_of_Representatives_elections,_2020"\n'
+    
+        with httmock.HTTMock(mock_requests):
+            states = load_states('http://example.com/states')
+        
+        self.assertEqual(len(states), 1)
+        self.assertIn(('Texas', 'House of Representatives'), states)
+        
+        state = states[('Texas', 'House of Representatives')]
+        self.assertEqual(state.state, 'Texas')
+        self.assertEqual(state.chamber, 'House of Representatives')
+        self.assertEqual(state.reason, 'Redistricting')
+        self.assertEqual(state.filing_deadline, datetime.date(2019, 12, 9))
+        self.assertEqual(state.primary_election, datetime.date(2020, 3, 3))
+        self.assertEqual(state.weight, 191333)
+        self.assertEqual(state.detail_url, 'https://ballotpedia.org/Texas_House_of_Representatives_elections,_2020')
 
 def parse_candidates(cell):
     ''' Return a list of Candidates for a cell value
@@ -41,3 +68,24 @@ def parse_date(cell):
     ''' Return a datetime.date object for a cell value
     '''
     return datetime.datetime.strptime(cell, '%B %d, %Y').date()
+
+def parse_number(cell):
+    '''
+    '''
+    return int(cell.replace(',', '') or '0')
+
+def load_states(url):
+    '''
+    '''
+    got = requests.get(url)
+    text = io.StringIO(got.text)
+    rows = csv.DictReader(text, dialect='excel')
+
+    return {
+        (row['State'], row['Chamber']): State(
+            row['State'], row['Chamber'], row['Reason'],
+            parse_date(row['Filing Deadline']), parse_date(row['Primary Election']),
+            parse_number(row['Weight']), row['Race Detail']
+            )
+        for row in rows
+        }
