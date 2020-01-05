805942bc829b35922ff94e673768d404755074ac
Michal Migurski <mike@teczno.com>
2020-01-05 14:31:27 -0800

    Added list of states

--- LiterallyAnyDemocrat/__init__.py
+++ LiterallyAnyDemocrat/__init__.py
@@ -8,7 +8,9 @@ CANDIDATES = data.load_candidates(os.environ['CANDIDATES_CSV_URL'])
 
 @app.route('/')
 def get_index():
-    return flask.render_template('index.html', candidates_url=flask.url_for('get_candidates_json'))
+    return flask.render_template('index.html',
+        states_url=flask.url_for('get_states_json'),
+        candidates_url=flask.url_for('get_candidates_json'))
 
 @app.route('/candidates.json')
 def get_candidates_json():
@@ -20,3 +22,13 @@ def get_candidates_json():
         'rows': [
         list(c) + list(STATES[(c.state, c.chamber)][2:-1]) for c in CANDIDATES
         ]})
+
+@app.route('/states.json')
+def get_states_json():
+    a_candidate = CANDIDATES[0]
+    a_state = STATES[(a_candidate.state, a_candidate.chamber)]
+
+    return flask.jsonify({
+        'head': list(a_state._fields),
+        'rows': [list(s) for s in STATES.values()],
+        })
